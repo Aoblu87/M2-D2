dce251ab570be2a341d6ee420c4f4dea427995f5
Stefania <stefania.struzzi@gmail.com>
2023-06-22 19:46:23 +0200

    completato

---
+++ script.js
@@ -0,0 +1,47 @@
+/*
+ESERCIZIO 1
+
+I principali datatype in javascript sono:
+
+- numeri interi negativi e positivi, numeri decimali;
+-stringhe, ossia insieme di caratteri alfanumerici che vengono inseriti all'interno di apici oppure virgolette;
+- booleani, che, a seguito di interrogazioni attraverso operatori di uguaglianza e differenza, danno come ridultato True e False;
+*/ 
+
+//ESERCIZIO 2
+
+let primoNumero= 12;
+let secondoNumero= 20;
+
+let risultato= 'Il risultato della somma tra 12 e 20 è '+ (primoNumero + secondoNumero);
+
+console.log(risultato);
+
+//ESERCIZIO 3
+
+let x=12;
+
+//ESERCIZIO 4
+
+let name= 'Stefania';
+
+//ESERCIZIO 5
+
+console.log(x-4);
+
+//ESERCIZIO 6
+
+let persona={
+    name:'Stefania',
+    cognome: 'Struzzi',
+    hobby: 'arrampicata',
+}
+
+// ESERCIZIO 7
+
+let name1='john';
+let name2='John';
+
+console.log(name1!==name2);
+
+console.log(name2.toLowerCase()==name1);
